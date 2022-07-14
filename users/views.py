b8abda5fb05b314eeb724ef0aeaae210181ed5a3
Ali Hamed Ali <59931761+alihamedali96@users.noreply.github.com>
2022-07-14 22:46:19 +0400

    Add new preference request

--- users/views.py
+++ users/views.py
@@ -18,3 +18,12 @@ def new_user(request):
     User.objects.create_user(
         username=user_information['name'], email=user_information['email'], password=user_information['password'])
     return JsonResponse({'message': 'user successfully created'})
+
+def new_pref(request):
+    pref_information = json.loads(request.body)
+    print(pref_information)
+    User.objects.create_pref(
+        user_id=pref_information['user_id'], diet_id=pref_information['diet_id'], meals_id=pref_information['meals_id'],
+        calores_limit=pref_information['calories_limit'], intolorences=pref_information['intolorences'], budget=pref_information['budget']
+        )
+    return JsonResponse({'message': 'Preferences successfully added'})
