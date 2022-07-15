f8992d104615afc65f180fbcfb997008ee1d3775
dartbill <billieldartnell@hotmail.com>
2022-07-15 08:29:33 +0100

    add user model to model.py

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import Preferences
+
+admin.site.register(Preferences)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from .models import Preferences
 import json
 
 # {
@@ -9,13 +10,13 @@ import json
 #     'password':'password'
 # }
 
-# this function creates a new user, it takes in name, email and password as json
 
-
+# home route
 def home(request):
     return JsonResponse({'message': 'Welcome to the server'})
 
 
+# this function creates a new user, it takes in name, email and password as json
 def new_user(request):
     user_information = json.loads(request.body)
     print(user_information)
@@ -24,10 +25,20 @@ def new_user(request):
     return JsonResponse({'message': 'user successfully created'})
 
 
+# def new_pref(request):
+#     pref_information = json.loads(request.body)
+#     print(pref_information)
+#     User.objects.create_pref(
+#         user_id=pref_information['user_id'], diet_id=pref_information['diet_id'], meals_id=pref_information['meals_id'],
+#         calores_limit=pref_information['calories_limit'], intolorences=pref_information[
+#             'intolorences'], budget=pref_information['budget']
+#     )
+#     return JsonResponse({'message': 'Preferences successfully added'})
+
 def new_pref(request):
     pref_information = json.loads(request.body)
     print(pref_information)
-    User.objects.create_pref(
+    Preferences.objects.create(
         user_id=pref_information['user_id'], diet_id=pref_information['diet_id'], meals_id=pref_information['meals_id'],
         calores_limit=pref_information['calories_limit'], intolorences=pref_information[
             'intolorences'], budget=pref_information['budget']
