de0c7b5d5b77f7857f0ab7212c76b9e143ce3b41
Ali Hamed Ali <59931761+alihamedali96@users.noreply.github.com>
2022-07-15 18:09:54 +0400

    add user test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from users.models import Diet, User
+from django.contrib.auth.models import User
+# from django.utils import unittest
 
-# Create your tests here.
+class DietTestCase(TestCase):
+    def setUp(self):
+        Diet.objects.create(vegan=False, vegetarian=False, gluten_free = False, ketogenic =False,pescetarian = False, peleo = True )
+        # User.objects.create_user(username="Ali")
+        self.user_1 = User.objects.create_user('Ali', 'ali@live.com', 'password')
+       
+
+    def test_user(self):
+        self.assertEqual(self.user_1.email, 'ali@live.com')
