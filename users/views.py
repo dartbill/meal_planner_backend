f836476aa790c4c0228d6cf80265894f45af306e
dartbill <billieldartnell@hotmail.com>
2022-07-22 06:56:47 +0100

    test

--- users/views.py
+++ users/views.py
@@ -16,7 +16,6 @@ def home(request):
 # login route
 def user_login(request):
     # gets response from FE
-
     user_information = json.loads(request.body)
     email = user_information['email']
     password = user_information['password']
@@ -26,8 +25,6 @@ def user_login(request):
     # check user exists
     if user is not None:
         login(request, user)
-        request.session['username'] = username
-        request.session['password'] = password
         return JsonResponse({'message': 'login successful'})
     else:
         return JsonResponse({'error': 'login unsuccessful'})
@@ -35,12 +32,11 @@ def user_login(request):
 
 # logout route
 def user_logout(request):
-
     # to be deleted when we can log in
-    user1 = authenticate(request, username='Layla', password='Something12')
+    user1 = authenticate(request, username='billie', password='Hello')
     if user1 is not None:
         login(request, user1)
-    #########
+    # #########
     logout(request)
     return JsonResponse({'message': 'User logged out'})
 
@@ -49,36 +45,20 @@ def user_logout(request):
 def new_user(request):
     # get information from FE
     user_information = json.loads(request.body)
-
+    # create user with data
     User.objects.create_user(
         username=user_information['name'], email=user_information['email'], password=user_information['password'])
-
-    password = user_information['password']
-    username = user_information['name']
-    user = authenticate(request, username=username, password=password)
-    login(request, user)
-
     return JsonResponse({'message': 'user successfully created'})
 
 
 # create preferences
-
-
 def create_prefs(request):
     # to be deleted when we can log in
-    # username = request.session.get('username')
-    # password = request.session.get('password')
-    # print(password)
-    # print(username)
-    # user1 = authenticate(request, username=username, password=password)
-    # if user1 is not None:
-    #     login(request, user1)
-    user1 = authenticate(request, username='Layla', password='Something12')
+    user1 = authenticate(request, username='billie', password='Hello')
     if user1 is not None:
         login(request, user1)
     #########
     # check if user is logged in
-
     if request.user.is_authenticated:
         # get data from FE
         information = json.loads(request.body)
@@ -124,72 +104,62 @@ def create_prefs(request):
 # update and get preferences
 def update_pref(request):
     # to be deleted when we can log in
-    # username = request.session.get('username')
-    # password = request.session.get('password')
-    # user1 = authenticate(request, username=username, password=password)
-    # if user1 is not None:
-    #     login(request, user1)
-    user1 = authenticate(request, username='Layla', password='Something12')
+    user1 = authenticate(request, username='billie', password='Hello')
     if user1 is not None:
         login(request, user1)
     #########
     # check if user is logged in
-
     if request.user.is_authenticated:
-
+        # get user information
         user = request.user
-        if request.method == 'PATCH':
 
+        if request.method == 'PATCH':
+            # get information from FE
             information = json.loads(request.body)
 
+            # Split data up
             pref_information = information['prefs']
             meal_info = information['meals']
             diet_information = information['diet']
 
+            # update preferences
             Preferences.objects.filter(user_id=user).update(calories_limit=pref_information['calories_limit'], intolorences=pref_information[
                 'intolorences'], budget=pref_information['budget'])
+
+            # update meals
             Meals.objects.filter(user_id=user).update(breakfast=meal_info['breakfast'], lunch=meal_info['lunch'], dinner=meal_info['dinner'],
                                                       dessert=meal_info['dessert'], snack=meal_info['snack'])
-            Diet.objects.filter(user_id=user).update(user_id=user, vegan=diet_information['vegan'], vegetarian=diet_information['vegetarian'],
-                                                     gluten_free=diet_information[
-                                                         'glutenfree'], ketogenic=diet_information['ketogenic'],
-                                                     pescetarian=diet_information['pescetarian'], paleo=diet_information['paleo'])
+
+            # update diet
+            Diet.objects.filter(user_id=user).update(user_id=user, vegan=diet_information['vegan'], vegetarian=diet_information['vegetarian'], gluten_free=diet_information['glutenfree'], ketogenic=diet_information['ketogenic'], pescetarian=diet_information[
+                'pescetarian'], paleo=diet_information['paleo'])
 
             return JsonResponse({'message': 'Preferences successfully updated'})
         elif request.method == "GET":
-            # get queries by values
             qs = Preferences.objects.filter(user_id=user).values(
                 'calories_limit', 'intolorences', 'budget')
             qs2 = Meals.objects.filter(user_id=user).values(
                 'breakfast', 'lunch', 'dinner', 'dessert', 'snack')
-            qs3 = Diet.objects.filter(user_id=user).values(
-                'vegan', 'vegetarian', 'gluten_free', 'ketogenic', 'pescetarian', 'paleo')
-            # chain the queries in the format FrontEnd requires
-            result_list = list(chain(qs, qs2, qs3))
+            result_list = list(chain(qs, qs2))
+            print(result_list)
             return JsonResponse(result_list, safe=False)
     else:
         return JsonResponse({'error': 'User not authenticated'})
 
 
 # set and get meal history
-
-
 def meal_history(request):
     # to be deleted when we can log in
-    # username = request.session.get('username')
-    # password = request.session.get('password')
-    # user1 = authenticate(request, username=username, password=password)
-    # if user1 is not None:
-    #     login(request, user1)
-    #########
-    # check if user is logged in
-    user1 = authenticate(request, username='Layla', password='Something12')
+    user1 = authenticate(request, username='billie', password='Hello')
     if user1 is not None:
         login(request, user1)
+    #########
+    # check if user is logged in
     if request.user.is_authenticated:
         # get information from FE
         if request.method == 'POST':
             meal_info = json.loads(request.body)
+            # print(meal_info['date'])
             # get user information
             user = request.user
             # create meal history object
@@ -201,6 +171,7 @@ def meal_history(request):
             user = request.user
             qs = MealHistory.objects.filter(
                 user_id=user).values('recipes', 'today_date')
+            print(qs)
             result_list = list(qs)
             return JsonResponse(result_list, safe=False)
     else:
@@ -209,18 +180,9 @@ def meal_history(request):
 
 # send email to user
 def send_email(request):
-    # username = request.session.get('username')
-    # password = request.session.get('password')
-    # print(password)
-    # print(username)
-    # user1 = authenticate(request, username=username, password=password)
-    # if user1 is not None:
-    #     login(request, user1)
-    #########
-    user1 = authenticate(request, username='Layla', password='Something12')
+    user1 = authenticate(request, username='billie', password='Hello')
     if user1 is not None:
         login(request, user1)
-    # check if user is logged ins
     if request.user.is_authenticated:
         user = request.user
         email_info = json.loads(request.body)
